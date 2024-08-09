d77a25aaae2662bfe0e891796f307bb682fa5bc6
Yibo Cao <cyb@ieee.org>
2024-08-08 20:06:08 -0400

    Fix generated textures getting stitched into non-block atlases

---
+++ src/main/java/net/deepacat/createpowerlines/mixins/DirectorySpriteSourceMixin.java
@@ -0,0 +1,34 @@
+package net.deepacat.createpowerlines.mixins;
+
+import net.deepacat.createpowerlines.blocks.connector.base.ConnectorMode;
+import net.deepacat.createpowerlines.blocks.connector.types.ConnectorSprite;
+import net.deepacat.createpowerlines.blocks.connector.types.ConnectorType;
+import net.deepacat.createpowerlines.blocks.connector.types.ConnectorTypes;
+import net.minecraft.client.renderer.texture.atlas.SpriteSource;
+import net.minecraft.client.renderer.texture.atlas.sources.DirectoryLister;
+import net.minecraft.server.packs.resources.ResourceManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DirectoryLister.class)
+public class DirectorySpriteSourceMixin {
+    @Final
+    @Shadow
+    private String sourcePath;
+
+    @Inject(method = "run", require = 1, at = @At("HEAD"))
+    private void createpowerlines$load(ResourceManager resMgr, SpriteSource.Output out, CallbackInfo ci) {
+        if (sourcePath.equals("block")) {
+            for (ConnectorType type : ConnectorTypes.TYPES) {
+                for (ConnectorMode mode : ConnectorMode.values()) {
+                    ConnectorSprite sprite = new ConnectorSprite(type, mode);
+                    out.add(sprite.loc, sprite);
+                }
+            }
+        }
+    }
+}
