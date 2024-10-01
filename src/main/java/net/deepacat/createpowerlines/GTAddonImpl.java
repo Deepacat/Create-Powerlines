7c1becdf69b0e34a0e28ca227299961986c7c672
Yibo Cao <cyb@ieee.org>
2024-09-30 21:10:56 -0400

    greg wires example

---
+++ src/main/java/net/deepacat/createpowerlines/GTAddonImpl.java
@@ -0,0 +1,31 @@
+package net.deepacat.createpowerlines;
+
+import com.gregtechceu.gtceu.api.GTCEuAPI;
+import com.gregtechceu.gtceu.api.addon.GTAddon;
+import com.gregtechceu.gtceu.api.addon.IGTAddon;
+import com.gregtechceu.gtceu.api.data.chemical.material.Material;
+import com.gregtechceu.gtceu.api.data.chemical.material.properties.PropertyKey;
+import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
+import net.deepacat.createpowerlines.item.WireMaterials;
+
+@GTAddon
+public class GTAddonImpl implements IGTAddon {
+    @Override
+    public String addonModId() {
+        return CreatePowerlines.MODID;
+    }
+
+    @Override
+    public GTRegistrate getRegistrate() {
+        return null;
+    }
+
+    @Override
+    public void initializeAddon() {
+        for (Material mat : GTCEuAPI.materialManager.getRegisteredMaterials()) {
+            if (mat.hasProperty(PropertyKey.WIRE)) {
+                WireMaterials.getOrRegister("GT " + mat.getName(), mat.getMaterialRGB());
+            }
+        }
+    }
+}
