55c2491252aaa47c31447bccd076fb8863cdb50e
Yibo Cao <cyb@ieee.org>
2024-11-22 22:54:17 -0500

    More cleanup

--- src/main/java/net/deepacat/createpowerlines/GTAddonImpl.java
+++ src/main/java/net/deepacat/createpowerlines/GTAddonImpl.java
@@ -8,6 +8,7 @@ import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.api.data.chemical.material.properties.PropertyKey;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
 import com.gregtechceu.gtceu.utils.GTUtil;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.deepacat.createpowerlines.blocks.connector.ConnectorType;
 import net.deepacat.createpowerlines.blocks.connector.ConnectorTypes;
 import net.deepacat.createpowerlines.config.Config;
@@ -26,6 +27,7 @@ public class GTAddonImpl implements IGTAddon {
             0x687ece, 0xd1d1d1, 0x323232, 0x578062, 0xFFFFFF, 0x54fc54, 0xfed947, 0x974451, 0x9f2db2, 0x5d5578};
 
     private ConnectorType[][] connectorTypes;
+    private Object2ObjectOpenHashMap<Material, WireMaterial> wireMats;
 
     @Override
     public String addonModId() {
@@ -39,29 +41,31 @@ public class GTAddonImpl implements IGTAddon {
 
     @Override
     public void initializeAddon() {
-        if (Config.USE_GT_CONNECTORS.get()) {
-            List<Material>[] tiers = (List<Material>[]) new List[MAX_SUPPORTED_TIER + 1];
-            for (int i = 0; i <= MAX_SUPPORTED_TIER; ++i) tiers[i] = new ArrayList<>();
+        if (!Config.USE_GT_CONNECTORS.get()) return;
 
-            // Converting gt materials with wire property to powerline wires
-            for (Material mat : GTCEuAPI.materialManager.getRegisteredMaterials()) {
-                if (mat.hasProperty(PropertyKey.WIRE)) {
-                    int tier = GTUtil.getTierByVoltage(mat.getProperty(PropertyKey.WIRE).getVoltage());
-                    if (tier <= MAX_SUPPORTED_TIER) tiers[tier].add(mat);
+        List<WireMaterial>[] tierWireMats = (List<WireMaterial>[]) new List[MAX_SUPPORTED_TIER + 1];
+        for (int i = 0; i <= MAX_SUPPORTED_TIER; ++i) tierWireMats[i] = new ArrayList<>();
+
+        // Converting GT materials with wire property to powerline wires
+        wireMats = new Object2ObjectOpenHashMap<>();
+        for (Material mat : GTCEuAPI.materialManager.getRegisteredMaterials()) {
+            if (mat.hasProperty(PropertyKey.WIRE)) {
+                int tier = GTUtil.getTierByVoltage(mat.getProperty(PropertyKey.WIRE).getVoltage());
+                if (tier <= MAX_SUPPORTED_TIER) {
+                    WireMaterial wireMat = WireMaterials.getOrRegister(
+                            WordUtils.capitalizeFully(mat.getName().replace("_", " ")), mat.getMaterialRGB());
+                    wireMats.put(mat, wireMat);
+                    tierWireMats[tier].add(wireMat);
                 }
             }
+        }
 
-            // Making connectors from wires
-            connectorTypes = new ConnectorType[MAX_SUPPORTED_TIER + 1][];
-            for (int i = 0; i <= MAX_SUPPORTED_TIER; ++i) {
-                if (!tiers[i].isEmpty()) {
-                    Material firstMat = tiers[i].get(0);
-                    long wireVoltage = firstMat.getProperty(PropertyKey.WIRE).getVoltage();
-                    List<WireMaterial> tierWireMats = tiers[i].stream().map(mat -> WireMaterials.getOrRegister(
-                            WordUtils.capitalizeFully(mat.getName().replace("_", " ")), mat.getMaterialRGB())).toList();
-                    connectorTypes[i] = ConnectorTypes.registerTier(GTValues.VN[i], wireVoltage, (i + 1) * 2 + 2, tierColors[i], tierWireMats);
-                }
-            }
+        // Making connectors from wires
+        connectorTypes = new ConnectorType[MAX_SUPPORTED_TIER + 1][];
+        for (int i = 0; i <= MAX_SUPPORTED_TIER; ++i) {
+            if (tierWireMats[i].isEmpty()) continue;
+            connectorTypes[i] = ConnectorTypes.registerTier(
+                    GTValues.VN[i], GTValues.V[i], (i + 1) * 2 + 2, tierColors[i], tierWireMats[i]);
         }
     }
 }
