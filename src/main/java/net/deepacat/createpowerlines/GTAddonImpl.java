0167a079d4b1695169f5d9235a74f3331c7720e8
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-07-07 03:57:02 -0500

    fix gt for 7.0.0 :P ! verbump + remove kjs plugin

--- src/main/java/net/deepacat/createpowerlines/GTAddonImpl.java
+++ src/main/java/net/deepacat/createpowerlines/GTAddonImpl.java
@@ -38,7 +38,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
-import static com.gregtechceu.gtceu.api.data.chemical.material.ItemMaterialData.ITEM_MATERIAL_INFO;
 import static com.gregtechceu.gtceu.api.data.chemical.material.ItemMaterialData.getMaterialInfo;
 
 @GTAddon
